38b671344f0fbaae875ec684d7f1db06f68cd72a
Mostafa-SAID7 <m.ssaid356@gmail.com>
2026-03-10 16:50:15 +0200

    Reorganize project structure: Move Migrations to Infrastructure/Data
    - Moved Migrations folder from Portfolio.API/Migrations to Portfolio.API/Infrastructure/Data/Migrations
    - This aligns with the project structure where Infrastructure contains Data access layer
    - All migration files are now properly organized under Infrastructure/Data/Migrations
    - Maintains clean separation of concerns with DbContext and migrations in the same Infrastructure/Data folder

---
+++ Portfolio.API/Infrastructure/Data/Migrations/20260310144311_CleanupOldTables.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanupOldTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Drop all old/obsolete tables that are no longer in the current model
+            migrationBuilder.Sql(@"
+                IF OBJECT_ID(N'[dbo].[AspNetUserLogins]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[AspNetUserLogins];
+                
+                IF OBJECT_ID(N'[dbo].[AspNetUserTokens]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[AspNetUserTokens];
+                
+                IF OBJECT_ID(N'[dbo].[AspNetUserClaims]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[AspNetUserClaims];
+                
+                IF OBJECT_ID(N'[dbo].[AspNetRoleClaims]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[AspNetRoleClaims];
+                
+                IF OBJECT_ID(N'[dbo].[AspNetUserRoles]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[AspNetUserRoles];
+                
+                IF OBJECT_ID(N'[dbo].[AspNetUsers]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[AspNetUsers];
+                
+                IF OBJECT_ID(N'[dbo].[AspNetRoles]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[AspNetRoles];
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
