f6d5146ef4e1513bfcd4def980f9ed73ddb6d426
Mostafa-SAID7 <m.ssaid356@gmail.com>
2026-03-12 09:49:21 +0200

    fix: add CancellationToken parameters to service interfaces
    - Add CancellationToken parameter to IServiceService methods
    - Add CancellationToken parameter to ICommentService methods
    - Align interface signatures with implementation signatures

--- Portfolio.API/Application/Features/Comments/Services/ICommentService.cs
+++ Portfolio.API/Application/Features/Comments/Services/ICommentService.cs
@@ -10,7 +10,7 @@ public interface ICommentService
 /// <param name="projectId">Identifier of the project to which the comment will be added.</param>
 /// <param name="request">Payload containing the comment content and related creation data.</param>
 /// <returns>A CommentDto representing the newly created comment.</returns>
-Task<CommentDto> AddCommentAsync(Guid projectId, CommentCreateDto request);
+Task<CommentDto> AddCommentAsync(Guid projectId, CommentCreateDto request, CancellationToken cancellationToken = default);
     /// <summary>
 /// Adds a reply to an existing comment on the specified project.
 /// </summary>
@@ -18,14 +18,14 @@ Task<CommentDto> AddCommentAsync(Guid projectId, CommentCreateDto request);
 /// <param name="commentId">The identifier of the parent comment to which the reply will be added.</param>
 /// <param name="request">The data for the reply to create.</param>
 /// <returns>The created reply as a <see cref="CommentDto"/>.</returns>
-Task<CommentDto> AddReplyAsync(Guid projectId, Guid commentId, CommentCreateDto request);
+Task<CommentDto> AddReplyAsync(Guid projectId, Guid commentId, CommentCreateDto request, CancellationToken cancellationToken = default);
     /// <summary>
 /// Adds a like to the specified comment within a project.
 /// </summary>
 /// <param name="projectId">The identifier of the project that contains the comment.</param>
 /// <param name="commentId">The identifier of the comment to like.</param>
 /// <returns>The updated total number of likes for the comment.</returns>
-Task<int> LikeCommentAsync(Guid projectId, Guid commentId);
+Task<int> LikeCommentAsync(Guid projectId, Guid commentId, CancellationToken cancellationToken = default);
 }
 
 
--- Portfolio.API/Application/Features/Services/Services/IServiceService.cs
+++ Portfolio.API/Application/Features/Services/Services/IServiceService.cs
@@ -8,32 +8,32 @@ public interface IServiceService
 /// Retrieves all service records.
 /// </summary>
 /// <returns>An enumerable of ServiceDto representing all services; empty if no services are found.</returns>
-Task<IEnumerable<ServiceDto>> GetServicesAsync();
+Task<IEnumerable<ServiceDto>> GetServicesAsync(CancellationToken cancellationToken = default);
     /// <summary>
 /// Retrieves a service by its identifier.
 /// </summary>
 /// <param name="id">The GUID identifier of the service to retrieve.</param>
 /// <returns>The matching <see cref="ServiceDto"/> if found; otherwise, <c>null</c>.</returns>
-Task<ServiceDto?> GetServiceByIdAsync(Guid id);
+Task<ServiceDto?> GetServiceByIdAsync(Guid id, CancellationToken cancellationToken = default);
     /// <summary>
 /// Creates a new service from the provided DTO and persists it.
 /// </summary>
 /// <param name="dto">Data transfer object containing the properties for the new service.</param>
 /// <returns>The created ServiceDto including any assigned identifiers or generated fields.</returns>
-Task<ServiceDto> CreateServiceAsync(ServiceDto dto);
+Task<ServiceDto> CreateServiceAsync(ServiceDto dto, CancellationToken cancellationToken = default);
     /// <summary>
 /// Updates the service identified by <paramref name="id"/> using values from <paramref name="dto"/> and returns the updated service.
 /// </summary>
 /// <param name="id">The identifier of the service to update.</param>
 /// <param name="dto">The DTO containing updated service properties.</param>
 /// <returns>The updated <see cref="ServiceDto"/>.</returns>
-Task<ServiceDto> UpdateServiceAsync(Guid id, ServiceDto dto);
+Task<ServiceDto> UpdateServiceAsync(Guid id, ServiceDto dto, CancellationToken cancellationToken = default);
     /// <summary>
 /// Deletes the service identified by the specified id.
 /// </summary>
 /// <param name="id">The unique identifier of the service to delete.</param>
 /// <returns>True if the service was deleted; false otherwise.</returns>
-Task<bool> DeleteServiceAsync(Guid id);
+Task<bool> DeleteServiceAsync(Guid id, CancellationToken cancellationToken = default);
 }
 
 
